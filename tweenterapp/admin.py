7d12bdf2aa78d143d6cc4244bbd6f54ca4d0a083
max_dev <max.santander@outlook.com>
2024-08-27 13:11:09 +0200

    First Commit and upload
    Signed-off-by: max_dev <max.santander@outlook.com>

---
+++ tweenterapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Profile, Post, Relationship
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Relationship)
